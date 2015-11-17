b6603f3bde45897b9684f943aa8bdb2e601a822d
Christopher Kruse <kruse.christopher@gmail.com>
2015-11-17 08:02:28 -0800

    removing build artifacts.

--- palindromer/target/debug/palindromer.d
+++
@@ -1,4 +0,0 @@
-/home/ckruse/Projects/rust/karan-projects/palindromer/target/debug/palindromer: main.rs
-
-/home/ckruse/Projects/rust/karan-projects/palindromer/target/debug/palindromer.d: main.rs
-
